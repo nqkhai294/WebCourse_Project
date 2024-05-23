d3abe8a1c286a322bc562a4de9e6ba13cf221975
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2024-05-24 00:37:30 +0700

    add Login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
+import Login from "./components/Login";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
+    <Login />
   )
 }
