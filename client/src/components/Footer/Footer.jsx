12621ed483124851c12bedf12ec7dfc597d874f9
phamson1511 <phamquyson2k4@gmail.com>
2024-06-03 21:12:50 +0700

    update footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,12 +2,41 @@ import "./Footer.css"
 
 const Footer = (props) => {
     return(
-        <div className="footer">
-            <div className="footer-content">2024 NextGen AI Inc. All rights reserved.</div>
-            <div className="footer-img">
-                Here is some img.
+        <footer>
+            <div className="row">
+            <div className="col">
+                    <h3>About Us</h3>
+                    <p className="aboutUs">
+                        The phrase "about us" is typically used to describe a section of a website or online presence that provides information about the entity behind it.
+                        This could be a company, organization, individual, or even a group of people working on a project.
+                        </p>
+                </div>
+                <div className="col"> 
+                    <h3>Office</h3>
+                    <p className="office"> Số 773/10 Nguyễn Duy Trinh, Phường Phú Hữu, Quận 9, Thành phố Hồ Chí Minh, Việt Nam </p>
+                    
+                </div>
+                
+                <div className="col">
+                    <h3>Developers</h3>
+                    <ul>
+                        <li>Ha Tien Dong</li>
+                        <li>Nguyen Quang Khai</li>
+                        <li>Pham Quy Son</li>
+                    </ul>
+                    
+                </div>
+                <div className="col">
+                    <h3>Contact </h3>
+                    <div className="hotline">1900 1900</div>
+                    <br />
+                    <img className="social" src="https://img.icons8.com/ios/100/facebook-new.png" alt="facebook-new"/>
+                    <img className="social" src="https://img.icons8.com/ios/100/twitter--v1.png" alt="twitter--v1"/>
+                    <img className="social" src="https://img.icons8.com/ios/100/instagram-new--v1.png" alt="instagram-new--v1"/>
+                    <img className="social" src="https://img.icons8.com/ios/100/pinterest--v1.png" alt="pinterest--v1"/>
+                </div>
             </div>
-        </div>
+        </footer>
     )
 }
 
