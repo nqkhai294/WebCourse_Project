45e6a11195cc8644d89ead4aa3a379f564704c42
phapsucongu <hatiendong2004@gmail.com>
2024-06-13 07:24:10 +0700

    add be link

--- client/src/constant/constants.js
+++ client/src/constant/constants.js
@@ -1,5 +1,5 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:1337/api' : 'https://yourdeployment.com/api';
-export const imgUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:1337' : 'https://yourdeployment.com';
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:1337/api' : 'https://wealthy-horn-134d2e87af.strapiapp.com/api';
+export const imgUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:1337' : 'https://wealthy-horn-134d2e87af.strapiapp.com';
 export const LOCAL_STORAGE_TOKEN_NAME = 'token';     
 import axios from 'axios';       
 const axiosInstance = axios.create({
